c37b46b82ddcac1755181ad3caaebaf1ba697920
LinuxMotion <jweyrauch359@gmail.com>
2011-07-10 20:34:31 -0700

    Remove formatting issue and potentially fix permissions.

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEVICE=facinatemtd
+DEVICE=fascinatemtd
 
 mkdir -p ../../../vendor/samsung/$DEVICE/proprietary
 
@@ -20,10 +20,10 @@ adb pull /system/lib/libsensor_yamaha_test.so  ../../../vendor/samsung/$DEVICE/p
 adb pull /system/bin/sensorcalibutil_yamaha ../../../vendor/samsung/$DEVICE/proprietary
 adb pull /system/bin/sensorserver_yamaha ../../../vendor/samsung/$DEVICE/proprietary
 adb pull /system/bin/sensorstatutil_yamaha ../../../vendor/samsung/$DEVICE/proprietary
-adb pull /system/lib/hw/sensors.default.so../../../vendor/samsung/$DEVICE/proprietary
+adb pull /system/lib/hw/sensors.default.so ../../../vendor/samsung/$DEVICE/proprietary
 
 
-(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/htc/$DEVICE/$DEVICE-vendor-blobs.mk
+(cat << EOF) | sed s/__DEVICE__/$DEVICE/g > ../../../vendor/samsung/$DEVICE/$DEVICE-vendor-blobs.mk
 # Copyright (C) 2010 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -40,29 +40,29 @@ adb pull /system/lib/hw/sensors.default.so../../../vendor/samsung/$DEVICE/propri
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \
-    vendor/samsung/fascinatemtd/proprietary/libcamera.so:obj/lib/libcamera.so \
-    vendor/samsung/fascinatemtd/proprietary/libril.so:obj/lib/libril.so \
-    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:obj/lib/libsecril-client.so
+    vendor/samsung/fascinatemtd/proprietary/libcamera.so:obj/lib/libcamera.so \\
+    vendor/samsung/fascinatemtd/proprietary/libril.so:obj/lib/libril.so \\
+    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:obj/lib/libsecril-client.so \
 
 
 # All the blobs necessary for galaxys devices
 PRODUCT_COPY_FILES += \
-    vendor/samsung/fascinatemtd/proprietary/libril.so:system/lib/libril.so \
-    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \
-    vendor/samsung/fascinatemtd/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \
-    vendor/samsung/fascinatemtd/proprietary/rild:system/bin/rild \
-    vendor/samsung/fascinatemtd/proprietary/libcamera.so:system/lib/libcamera.so \
-    vendor/samsung/fascinatemtd/proprietary/pppd_runner:system/bin/pppd_runner \
-    vendor/samsung/fascinatemtd/proprietary/gpsd:system/vendor/bin/gpsd \
-    vendor/samsung/fascinatemtd/proprietary/gps.xml:system/vendor/etc/gps.xml \
-    vendor/samsung/fascinatemtd/proprietary/gps.aries.so:system/lib/hw/gps.aries.so \
-    vendor/samsung/fascinatemtd/proprietary/libms3c_yamaha.so:system/lib/libms3c_yamaha.so \
-    vendor/samsung/fascinatemtd/proprietary/libsensor_yamaha.so:system/lib/libsensor_yamaha.so \
-    vendor/samsung/fascinatemtd/proprietary/libsensor_yamaha_test.so:system/lib/libsensor_yamaha_test.so \
-    vendor/samsung/fascinatemtd/proprietary/sensorcalibutil_yamaha:system/bin/sensorcalibutil_yamaha \
-    vendor/samsung/fascinatemtd/proprietary/sensorserver_yamaha:system/bin/sensorserver_yamaha \
-    vendor/samsung/fascinatemtd/proprietary/sensorstatutil_yamaha:system/bin/sensorstatutil_yamaha \
-    vendor/samsung/fascinatemtd/proprietary/sensors.default.so:system/lib/hw/sensors.default.so
+    vendor/samsung/fascinatemtd/proprietary/libril.so:system/lib/libril.so \\
+    vendor/samsung/fascinatemtd/proprietary/libsecril-client.so:system/lib/libsecril-client.so \\
+    vendor/samsung/fascinatemtd/proprietary/libsec-ril40.so:system/lib/libsec-ril40.so \\
+    vendor/samsung/fascinatemtd/proprietary/rild:system/bin/rild \\
+    vendor/samsung/fascinatemtd/proprietary/libcamera.so:system/lib/libcamera.so \\
+    vendor/samsung/fascinatemtd/proprietary/pppd_runner:system/bin/pppd_runner \\
+    vendor/samsung/fascinatemtd/proprietary/gpsd:system/vendor/bin/gpsd \\
+    vendor/samsung/fascinatemtd/proprietary/gps.xml:system/vendor/etc/gps.xml \\
+    vendor/samsung/fascinatemtd/proprietary/gps.aries.so:system/lib/hw/gps.aries.so \\
+    vendor/samsung/fascinatemtd/proprietary/libms3c_yamaha.so:system/lib/libms3c_yamaha.so \\
+    vendor/samsung/fascinatemtd/proprietary/libsensor_yamaha.so:system/lib/libsensor_yamaha.so \\
+    vendor/samsung/fascinatemtd/proprietary/libsensor_yamaha_test.so:system/lib/libsensor_yamaha_test.so \\
+    vendor/samsung/fascinatemtd/proprietary/sensorcalibutil_yamaha:system/bin/sensorcalibutil_yamaha \\
+    vendor/samsung/fascinatemtd/proprietary/sensorserver_yamaha:system/bin/sensorserver_yamaha \\
+    vendor/samsung/fascinatemtd/proprietary/sensorstatutil_yamaha:system/bin/sensorstatutil_yamaha \\
+    vendor/samsung/fascinatemtd/proprietary/sensors.default.so:system/lib/hw/sensors.default.so \
 
 EOF
 
